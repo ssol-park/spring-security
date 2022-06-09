f6ca9f26271734d3b07d0c46e6ec3004793032af
ssol-park <ksttlov.naver@hanmail.net>
2022-06-09 17:11:00 +0900

    Feat: added passwordEncoder

--- src/main/java/com/ssolpark/security/config/BeanConfig.java
+++ src/main/java/com/ssolpark/security/config/BeanConfig.java
@@ -3,6 +3,8 @@ package com.ssolpark.security.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeanConfig {
@@ -10,6 +12,11 @@ public class BeanConfig {
     @Bean
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
