3607484610d9758f09896e9c8504536d2a870e87
ssol-park <ksttlov.naver@hanmail.net>
2022-07-06 17:53:25 +0900

    Feat: added RestTemplate

--- src/main/java/com/ssolpark/security/config/BeanConfig.java
+++ src/main/java/com/ssolpark/security/config/BeanConfig.java
@@ -1,10 +1,18 @@
 package com.ssolpark.security.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssolpark.security.interceptor.HttpClientLoggingInterceptor;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @Configuration
 public class BeanConfig {
@@ -17,6 +25,26 @@ public class BeanConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        HttpClient client = HttpClientBuilder.create()
+                .setMaxConnTotal(50)
+                .setMaxConnPerRoute(20)
+                .build();
+
+        factory.setHttpClient(client);
+        factory.setConnectTimeout(2000);
+        factory.setReadTimeout(5000);
+
+        RestTemplate restTemplate = new RestTemplate(new BufferingClientHttpRequestFactory(factory));
+        restTemplate.setInterceptors(Collections.singletonList(new HttpClientLoggingInterceptor()));
+
+        return restTemplate;
     }
 
 }
---
+++ src/main/java/com/ssolpark/security/interceptor/HttpClientLoggingInterceptor.java
@@ -0,0 +1,20 @@
+package com.ssolpark.security.interceptor;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.IOException;
+
+public class HttpClientLoggingInterceptor implements ClientHttpRequestInterceptor {
+
+    //todo
+    @Override
+    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+
+        ClientHttpResponse response = execution.execute(request, body);
+
+        return response;
+    }
+}
