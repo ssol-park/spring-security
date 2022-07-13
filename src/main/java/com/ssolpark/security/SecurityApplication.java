a95bd5f7349ccd4f96b2363232cd2cf415c8d579
ssol-park <ksttlov.naver@hanmail.net>
2022-07-13 18:49:16 +0900

    Feat: added redis config

--- src/main/java/com/ssolpark/security/SecurityApplication.java
+++ src/main/java/com/ssolpark/security/SecurityApplication.java
@@ -2,7 +2,9 @@ package com.ssolpark.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class SecurityApplication {
 
---
+++ src/main/java/com/ssolpark/security/constant/CacheKey.java
@@ -0,0 +1,9 @@
+package com.ssolpark.security.constant;
+
+public class CacheKey {
+
+    public static final String USER = "user";
+
+    public static final int DEFAULT_EXPIRE_SEC = 60;
+
+}
